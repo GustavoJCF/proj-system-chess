f327217d8531f3040743f65de7d40f58f58227a8
Gustavo Falcão <gustavojcfalcao@gmail.com>
2026-07-27 20:38:34 -0300

    First class: Position

---
+++ src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position opa = new Position(1, 3);
+        System.out.println(opa);
+    }
+}
